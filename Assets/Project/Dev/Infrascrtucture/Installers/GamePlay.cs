d73144c070542c57953deba58251432f0d3902fa
rodion3000 <102345066+rodion3000@users.noreply.github.com>
2025-05-05 14:17:49 +0500

    1

--- Assets/Project/Dev/Infrascrtucture/Installers/GamePlay.cs
+++ Assets/Project/Dev/Infrascrtucture/Installers/GamePlay.cs
@@ -1,17 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
-public class GamePlay : MonoBehaviour
+public class GamePlay : MonoInstaller
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public override void InstallBindings()
     {
         
     }
