7de30dce24d0fb5ec5fe8d913e1ed8cbf4d1dff2
rodion3000 <102345066+rodion3000@users.noreply.github.com>
2025-05-10 01:01:09 +0500

    1

--- Assets/Project/Dev/Infrascrtucture/Installers/GamePlayInstaller.cs
+++ Assets/Project/Dev/Infrascrtucture/Installers/GamePlayInstaller.cs
@@ -5,8 +5,18 @@ using Zenject;
 
 public class GamePlayInstaller : MonoInstaller
 {
+    [SerializeField] private GameObject playerPrefab;
     public override void InstallBindings()
     {
-        
+        BindPlayer();
+    }
+
+    private void BindPlayer()
+    {
+        Container.BindInterfacesAndSelfTo<SpineArcher>()
+            .FromComponentInNewPrefab(playerPrefab)
+            .WithGameObjectName("Player")
+            .UnderTransformGroup("Gameplay")
+            .AsSingle().NonLazy();
     }
 }
