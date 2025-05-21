fa96af840f1adab51d9a59e5375c85fdf503ca17
rodion3000 <rodion.papizh@mail.ru>
2025-05-21 13:58:51 +0500

    1

--- Assets/Project/Dev/Infrascrtucture/Installers/ProjectInstallers/GameStateMachineInstaller.cs
+++ Assets/Project/Dev/Infrascrtucture/Installers/ProjectInstallers/GameStateMachineInstaller.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Project.Dev.Infrastructure.GameStateMachine.States;
 using UnityEngine;
 using Zenject;
 
-public class GameStateMachineInstaller : MonoInstaller
+namespace Project.Dev.Infrascrtucture.Installers.ProjectInstallers
 {
-    public override void InstallBindings()
+    public class GameStateMachineInstaller : MonoInstaller
     {
-       
+        public override void InstallBindings()
+        {
+            Container.Bind<BootstrapState>().AsSingle().NonLazy();
+            Container.Bind<GameLoopState>().AsSingle().NonLazy();
+            Container.Bind<GamePauseState>().AsSingle().NonLazy();
+            Container.Bind<LoadLevelState>().AsSingle().NonLazy();
+            Container.Bind<LoadMetaState>().AsSingle().NonLazy();
+            Container.Bind<LoadProgresState>().AsSingle().NonLazy();
+
+            Container.BindInterfacesAndSelfTo<GameStateMachine>().AsSingle();
+        }
     }
 }
+
